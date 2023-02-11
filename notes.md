ccc6238991c3c484d9ae3c0e98c750cb2bc4ea73
Pankaj-Surya <pnkjsrvnsh@gmail.com>
2023-02-11 17:07:21 +0530

    completed basic sequelize setup for create a db

---
+++ notes.md
@@ -0,0 +1,8 @@
+# Configuratation
+1. Setup sever and dotenv 
+2. Seting up seqelize
+     -npx sequelize init => Created "config\config.json"
+     -Go first inside src becoz we have config,json inside src 
+        -npx sequelize db:create
+3. 
+     
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const {PORT} = require('./config/serverConfig')
+const bodyParser = require("body-parser");
 
 const setupAndStartServer = async ()=>{
-
-    //console.log(process.env)
+    // create the express object
     const app = express();
-    //const PORT = process.env.PORT;
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+
     app.listen(PORT,()=>{
         console.log(`Server start at ${PORT}`);
     });
